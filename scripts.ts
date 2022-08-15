006f07ee28ac3e6a9bf7303a73757a9f55454eed
flaaash <flaaash@gmail.com>
2022-08-15 16:14:45 -0400

    change: User.userReference as foraign key

--- scripts.ts
+++ scripts.ts
@@ -12,6 +12,20 @@ const main = async () => {
   // const users = await prisma.user.findMany();
   // console.log(users);
   await prisma.user.deleteMany();
+  const user = await prisma.user.create({
+    data: {
+      name: "alex",
+      birthday: new Date("1978-09-22"),
+      email: "alex@test.com",
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+    include: { userPreference: true },
+  });
+  console.log(user);
 };
 
 main()
